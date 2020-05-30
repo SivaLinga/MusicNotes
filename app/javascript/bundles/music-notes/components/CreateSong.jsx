9f0868c0ea9cb16d5072eb3c5c27d6f41076cbbc
Linga, SivaramaKrishna <sivaramakrishna.linga@cerner.com>
2020-05-29 23:38:18 -0500

    removing helloworld files

---
+++ app/javascript/bundles/music-notes/components/CreateSong.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CreateSong = () => {
+  return <div>Hello</div>
+};
+
+export default CreateSong;
---
+++ app/javascript/packs/music-notes-bundle.js
@@ -0,0 +1,7 @@
+import ReactOnRails from 'react-on-rails';
+
+import CreateSong from '../bundles/music-notes/components/CreateSong';
+
+ReactOnRails.register({
+  CreateSong,
+});
